b03946e88f1ce32ca6eb7d4356bcec12835966ab
ffutop <17816873960@163.com>
2025-11-23 10:35:31 +0800

    fix: multi-arch support

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -2,10 +2,22 @@
 #Create docker image for input file.
 #Target is a build with libc
 #Save docker image as tar
+
+ARCH=${2:-amd64}
+
+if [ "$ARCH" = "amd64" ]; then
+    RUST_TARGET="x86_64-unknown-linux-musl"
+elif [ "$ARCH" = "arm64" ]; then
+    RUST_TARGET="aarch64-unknown-linux-musl"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 echo $1
 docker image rm $1
-cp ../target/x86_64-unknown-linux-musl/release/$1 .
+cp ../target/${RUST_TARGET}/release/$1 .
 docker build -t $1 . --build-arg file=$1
 rm $1
-mkdir dockers
+mkdir -p dockers
 docker save $1 > dockers/$1.tar
--- scripts/create_tars.sh
+++ scripts/create_tars.sh
@@ -16,11 +16,11 @@ if [ "${SKIP_RUST_BUILD}" = "true" ]; then
     echo ">>> SKIP_RUST_BUILD is set to true. Using pre-built binaries..."  
 else  
     BUILDER=${RUST_BUILDER:-cargo}  
-    echo ">>> Building with $$BUILDER for target $$RUST_TARGET..."  
-    $$BUILDER build --target $${RUST_TARGET} --release  
+    echo ">>> Building with $BUILDER for target $RUST_TARGET..."  
+    $BUILDER build --target ${RUST_TARGET} --release  
 fi  
 
-./create_images.sh
+./create_images.sh $ARCH
 
 tar -czvf osdd_ingress-${ARCH}.tar.gz osdd.service ../target/${RUST_TARGET}/release/osdd ../settings/ingress/Config.toml dockers/ph_kafka_ingress.tar dockers/transport_udp_send.tar  dockers/ph_mock_ingress.tar dockers/ph_udp_ingress.tar dockers/filter.tar
 tar -czvf osdd_egress-${ARCH}.tar.gz osdd.service ../target/${RUST_TARGET}/release/osdd ../settings/egress/Config.toml dockers/ph_kafka_egress.tar  dockers/transport_udp_receive.tar dockers/ph_mock_egress.tar dockers/ph_udp_egress.tar dockers/filter.tar
